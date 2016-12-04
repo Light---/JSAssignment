fdbfc32616587491c566cd791ba005a9286ba05a
Light--- <eleary01@qub.ac.uk>
2016-12-04 22:19:27 +0000

    Add 7 valid array functions

--- app/arrays.js
+++ app/arrays.js
@@ -2,31 +2,75 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
   indexOf: function(arr, item) {
-
+    for (var i = 0; i<arr.length; i++){
+      if (arr[i] === item){
+        return i;
+      }
+    }
+    return -1;
   },
 
   sum: function(arr) {
+    var answer = 0;
+
+    for (var i = 0; i<arr.length; i++){
+      answer = answer + arr[i];
+    }
+
+    return answer;
 
   },
 
   remove: function(arr, item) {
+    var answer = [];
 
+    for (var i = 0; i<arr.length; i++){
+      if (arr[i] !== item){
+        answer.push(arr[i]);
+      }
+    }
+
+    return answer;
   },
 
   removeWithoutCopy: function(arr, item) {
+    for (var i = 0; i<arr.length; i++){
+      if (arr[i] === item){
+        arr.splice(i, 1)
+        i--;
+      }
+    }
 
+    return arr;
   },
 
   append: function(arr, item) {
+    var newArray = [];
 
+    for (var i = 0; i<arr.length; i++){
+      newArray.push(arr[i])
+    }
+    newArray.push(item);
+    return newArray;
   },
 
   truncate: function(arr) {
+    var newArray = [];
 
+    for (var i = 0; i<arr.length-1; i++){
+      newArray.push(arr[i])
+    }
+
+    return newArray;
   },
 
   prepend: function(arr, item) {
+    var newArray = [item];
 
+    for (var i = 0; i<arr.length; i++){
+      newArray.push(arr[i])
+    }
+    return newArray;
   },
 
   curtail: function(arr) {
